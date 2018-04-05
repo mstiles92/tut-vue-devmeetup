58dd61f38b391a9761d59de400ec61115b89b083
Matthew Stiles <mstiles92@gmail.com>
2018-04-05 17:57:14 -0400

    Part 6

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,61 @@
 <template>
-    <div>
-        <p>The Home Page</p>
-    </div>
+    <v-container>
+        <v-layout row wrap>
+            <v-flex xs12 sm6 class="text-xs-center text-sm-right">
+                <v-btn large router to="/meetups" class="info">Explore Meetups</v-btn>
+            </v-flex>
+            <v-flex xs12 sm6 class="text-xs-center text-sm-left">
+                <v-btn large router to="/meetup/new" class="info">Organize Meetup</v-btn>
+            </v-flex>
+        </v-layout>
+        <v-layout row wrap class="mt-2">
+            <v-flex xs12>
+                <v-carousel>
+                    <v-carousel-item v-for="meetup in meetups" :src="meetup.imageUrl" :key="meetup.id">
+                        <div class="title">{{ meetup.title }}</div>
+                    </v-carousel-item>
+                </v-carousel>
+            </v-flex>
+        </v-layout>
+        <v-layout row wrap class="mt-2">
+            <v-flex xs12 class="text-xs-center">
+                <p>Join our awesome meetups!</p>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
     export default {
-        name: 'Home'
+        name: 'Home',
+        data() {
+            return {
+                meetups: [
+                    {
+                        imageUrl: 'https://i.ytimg.com/vi/MtCMtC50gwY/maxresdefault.jpg',
+                        id: '0001',
+                        title: 'New York Meetup'
+                    },
+                    {
+                        imageUrl: 'https://i.ytimg.com/vi/_FYKIhJZdaI/maxresdefault.jpg',
+                        id: '0002',
+                        title: 'Paris Meetup'
+                    }
+                ]
+            }
+        }
     }
 </script>
 
 <style scoped>
-
+    .title {
+        position: absolute;
+        left: 50%;
+        transform: translate(-50%, 0);
+        bottom: 50px;
+        background-color: rgba(0,0,0,0.5);
+        color: white;
+        font-size: 2em;
+        padding: 20px;
+    }
 </style>
