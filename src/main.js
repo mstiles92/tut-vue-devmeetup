f098c4f1b297ea0ad9046dc53b6c167222b3702f
Matthew Stiles <mstiles92@gmail.com>
2018-04-05 16:19:20 -0400

    Part 4

--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@ import App from './App'
 import router from './router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import colors from 'vuetify/es5/util/colors'
 
-Vue.use(Vuetify)
+Vue.use(Vuetify, {
+    theme: {
+        primary: colors.orange.base,
+        secondary: colors.orange.lighten3,
+        accent: colors.lightGreen.lighten1
+    }
+})
 
 Vue.config.productionTip = false
 
