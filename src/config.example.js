36294188a05593ff39d6b1ba3cbb213b8bf830a6
mstiles92 <mstiles92@gmail.com>
2018-04-22 01:25:44 -0400

    Part 14

---
+++ src/config.example.js
@@ -0,0 +1,9 @@
+export default {
+    // Enter the values from your Firebase Web Setup dialog
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
+}
--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App'
+import * as firebase from 'firebase'
 import router from './router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import colors from 'vuetify/es5/util/colors'
 import { store } from './store'
 import DateFilter from './filters/date'
+import * as firebaseConfig from './config'
 
 Vue.use(Vuetify, {
     theme: {
@@ -24,5 +26,8 @@ new Vue({
     el: '#app',
     router,
     store,
-    render: h => h(App)
+    render: h => h(App),
+    created() {
+        firebase.initializeApp(firebaseConfig)
+    }
 })
